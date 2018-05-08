5817a1bfea1c4887366b9e8c1d62c5c0a7d72010
hua.zhou <hua.zhou@clobotics.com>
2018-05-08 13:31:15 +0800

    m950_sitl: "make posix gazebo_m950_v6_v5"

--- Tools/uorb_graph/create_from_startupscript.sh
+++ Tools/uorb_graph/create_from_startupscript.sh
@@ -3,7 +3,8 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-startup_file="$SCRIPT_DIR"/../../posix-configs/SITL/init/ekf2/typhoon_h480
+startup_file="$SCRIPT_DIR"/../../posix-configs/SITL/init/ekf2/m950_v6_v5
+#startup_file="$SCRIPT_DIR"/../../posix-configs/SITL/init/ekf2/typhoon_h480
 [ -n "$1" ] && startup_file=$1
 # get the modules as comma-separated list
 modules=$(cat "$startup_file"|cut -f1 -d' '|sort|uniq|tr '\n' ,)
